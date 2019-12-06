5b96f638eba2aacbb64965ae876bfa91aef153d3
Denis <gungstar1986@gmail.com>
2019-12-06 08:25:48 +0200

    Updates

---
+++ precache-manifest.2abba97eb0ce6d83a426486e32207de2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b6d9bf99b6b28227d43e733fe01cb3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bab5ff661f9fdb5b0f21",
+    "url": "/static/css/main.538f879b.chunk.css"
+  },
+  {
+    "revision": "7dc38f90097efc299070",
+    "url": "/static/js/2.0159c9f1.chunk.js"
+  },
+  {
+    "revision": "bab5ff661f9fdb5b0f21",
+    "url": "/static/js/main.d921700a.chunk.js"
+  },
+  {
+    "revision": "3d54a4f227ea9df2cccc",
+    "url": "/static/js/runtime-main.ba9f847d.js"
+  }
+]);
